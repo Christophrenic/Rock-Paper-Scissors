fc0cc7a964909795ef1244c645d9658b195dd1fc
Christophrenic <panth300@gmail.com>
2022-02-22 16:13:44 +0100

    ADDED COMMENTS

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 
-function computerPlay() {
+function computerPlay() {                                 // Makes choice randomly from the array and returns it.
 return array[Math.floor(Math.random() * array.length)];
 }
 
-function userPlay() {
+function userPlay() {                                    // Collects user input from prompt and returns it.
 return prompt('Choose: Rock, Paper or Scissors', 'Choose it! NOW!!');
 }
 
-function evaluation(playerselection, computerselection) {
+function evaluation(playerselection, computerselection) { 
     if (playerselection.toLowerCase() === computerselection.toLowerCase()) {
         console.log('Draw!');
         draw++;
-
     } else if (
-    (playerselection.toLowerCase() == "rock" && computerselection.toLowerCase() == "scissors") ||      //VICTORIES + SCORING
+    (playerselection.toLowerCase() == "rock" && computerselection.toLowerCase() == "scissors") ||      //Evaluates winner and adds to either playerscore or computerscore or draw.
     (playerselection.toLowerCase() == "paper" && computerselection.toLowerCase() == "rock") || 
     (playerselection.toLowerCase() == "scissors" && computerselection.toLowerCase() == "paper")) {
         console.log("You win!");
@@ -28,10 +27,10 @@ function evaluation(playerselection, computerselection) {
 function game() {
     let i = 0;
     while (i < 5) {
-        evaluation(userPlay(), computerPlay());
+        evaluation(userPlay(), computerPlay()); //This collects return from userPlay & computerPLay and takes their return values as paramaters for evaluation function.
         i++;
     } if (playerScore > computerScore) {
-        alert(`You win! Your score: ${playerScore} Computer score: ${computerScore} Draws: ${draw}`)
+        alert(`You win! Your score: ${playerScore} Computer score: ${computerScore} Draws: ${draw}`)            //Plays 5 rounds of the game and declares winner and loser.
         return;
     } else if (computerScore > playerScore); {
         alert(`You lose! Your score: ${playerScore}, Computer score: ${computerScore} Draws: ${draw}`);
