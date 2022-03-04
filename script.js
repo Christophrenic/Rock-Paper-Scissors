4b3ced16868c858f4c278212a59113dc5ad2f112
Christophrenic <panth300@gmail.com>
2022-03-04 15:12:30 +0100

    WORKING SCORE
    Added counter to if statements that increase and display player & computer score.

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ let playerScore = 0;
 let computerScore = 0;
 let draw = 0;
 let playerselection = '';
-const array = ["rock", "paper", "scissors"];
+
+let displayPlayerScore = document.querySelector(".dashboard .playerscore") //Displays score
+displayPlayerScore.textContent = `Your score: ${playerScore}`
+
+let displayComputerScore = document.querySelector(".dashboard .computerscore")
+displayComputerScore.textContent = `Computer score: ${computerScore}`
+
+let drawScore = document.querySelector(".dashboard .draw")
+drawScore.textContent = `Draw: ${draw}`
+
+
+const array = ["rock", "paper", "scissors"];      //Array is created and computer makes selection
 let computerselection = array[Math.floor(Math.random() * array.length)];
 
 
-const rockBtn = document.querySelector('#rock');
+const rockBtn = document.querySelector('#rock');  //Player selection based on button + plays the game
 rockBtn.addEventListener('click', function(e) {
     playerselection = 'rock';
     evaluation(playerselection, computerselection);
@@ -33,6 +44,7 @@ function evaluation() {
     if (playerselection === computerselection) {
         console.log('Draw!');
         draw++;
+        drawScore.textContent = `Draw: ${draw}`
         
     } else if (
     (playerselection == "rock" && computerselection == "scissors") ||      //Evaluates winner and adds to either playerscore or computerscore or draw.
@@ -40,10 +52,11 @@ function evaluation() {
     (playerselection == "scissors" && computerselection == "paper")) {
         console.log("You win!");
         playerScore++;
+        displayPlayerScore.textContent = `Your score: ${playerScore}`
 
     } else {
         console.log("You lose");
         computerScore++
+        displayComputerScore.textContent = `Computer score: ${computerScore}`
     }
 }
-
